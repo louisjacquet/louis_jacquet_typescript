60f93b68fb32d7aa48a2584965633197451d2c39
louisjacquet <louisjacquet0705@gmail.com>
2022-05-16 17:28:46 +0200

    Toolbox part 1

---
+++ src/pages/Toolbox/style.tsx
@@ -0,0 +1,100 @@
+import { StyleSheet } from "react-native";
+export const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    marginTop: 12,
+  },
+
+  button: {
+    backgroundColor: "#61C3D7",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 3,
+    height: 45,
+    padding: 15,
+    width: 120,
+  },
+
+  text1: {
+    fontSize: 24,
+    color: "#E4E4E4",
+    fontWeight: "300", //Semibold
+    left: 190,
+  },
+  text1b: {
+    fontSize: 24,
+    marginLeft: 5,
+    fontWeight: "300",
+    left: 190,
+  },
+  text2: {
+    fontSize: 14,
+    fontWeight: "600", //Semibold
+    top: 5,
+    left: 210,
+  },
+  textdiv: {
+    fontSize: 12,
+    fontWeight: "800", //Semibold
+  },
+
+  text3: {
+    fontSize: 17,
+    textAlign: "center",
+    fontWeight: "400",
+    color: "#FFFFFF",
+    width: 100,
+  },
+
+  textg: {
+    fontSize: 25,
+    fontWeight: "600",
+    textAlign: "center",
+    color: "#FFFFFF",
+  },
+  CardRactangle: {
+    marginBottom: 5,
+    marginTop: 10,
+    shadowColor: "#000000",
+    shadowOpacity: 0.15,
+    shadowRadius: 10,
+    width: "80%",
+    height: "60%",
+    marginLeft: "15%",
+    marginRight: "5%",
+    backgroundColor: "#FFFFFF",
+  },
+  CardRactangle2: {
+    marginBottom: 5,
+    marginTop: 10,
+    shadowColor: "#000000",
+    shadowOpacity: 0.15,
+    shadowRadius: 10,
+    width: "80%",
+    height: "60%",
+    marginLeft: "10%",
+    marginRight: "10%",
+    backgroundColor: "#FFFFFF",
+  },
+  button2: {
+    backgroundColor: "#26AA90",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 3,
+    height: 45,
+    padding: 15,
+    width: 120,
+  },
+
+  roundButton1: {
+    width: 30,
+    height: 30,
+    textAlign: "center",
+    justifyContent: "flex-end",
+    padding: 15,
+    borderRadius: 100,
+    backgroundColor: "#61C3D7",
+    borderColor: "#E3EFF2",
+    borderWidth: 5,
+  },
+});
